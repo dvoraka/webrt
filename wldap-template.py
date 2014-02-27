4588b769e4e489b7c8c5f6b7b50015886d19c069
Alen Dvorak <alen.dvorak@gmail.com>
2014-02-27 16:01:50 +0100

    Improve LDAP template

---
+++ wldap-template.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import ldap
+
+class LDAP:
+    
+    def bind(self, user, password):
+        pass
+
+    def check_password(self, login, password):
+        
+        return True
+
+    def is_staff(self, cid):
+        
+        return True
