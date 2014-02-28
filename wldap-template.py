4151395665caff72464a9fdfecf3541d2899b645
Alen Dvorak <alen.dvorak@gmail.com>
2014-02-28 11:30:50 +0100

    Improve LDAP template

--- wldap-template.py
+++ wldap-template.py
@@ -4,6 +4,9 @@ import ldap
 
 
 class LDAP:
+    '''Template LDAP class. Custom backend use this class, so it is good
+    to have minimal implementation of it in case you want to use default
+    CustomBackend class.'''
     
     def __init__(self, host='ldaps://localhost'):
         
