b9abb0825f797d36a3359ef08b29aae899a1cfef
Alen Dvorak <alen.dvorak@gmail.com>
2014-03-01 14:40:09 +0100

    Improve install script

--- install_helper.sh
+++ install_helper.sh
@@ -3,6 +3,7 @@
 # install_helper.sh
 #
 # Helps with WebRT installation steps.
+#
 
 
 WEBRT_GIT=https://github.com/dvoraka/webrt.git
@@ -28,4 +29,51 @@ function check_git() {
 
 }
 
-check_git
+function install_git() {
+
+    debian_install_git
+
+}
+
+function debian_install_git() {
+
+    apt-get install git-core
+
+}
+
+function check_RT4() {
+
+    if [ -d /etc/request-tracker4 ]
+    then
+        echo 'RT4 OK'
+        return 0
+    else
+        echo "You don't have RT4."
+        return 1
+    fi
+
+}
+
+function install_RT4() {
+
+    debian_install_RT4
+
+}
+
+function debian_install_RT4() {
+
+    apt-get install request-tracker4
+
+}
+
+function main() {
+
+    check_git
+    check_RT4
+    #install_git
+    #install_RT4
+
+}
+
+
+main
