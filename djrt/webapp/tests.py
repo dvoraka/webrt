5f25d9517e67592c8921a0b47d4111a44fde67ff
Alen Dvorak <alen.dvorak@gmail.com>
2014-03-06 17:08:12 +0100

    Add tests

---
+++ djrt/webapp/tests.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from django.test import LiveServerTestCase
+from django.conf import settings
+
+
+class SimpleSeleniumTest(LiveServerTestCase):
+
+    def setUp(self):
+
+        self.browser = webdriver.Firefox()
+        #settings.DEBUG = True
+
+    def tearDown(self):
+
+        self.browser.quit()
+
+    def test_login(self):    
+
+        self.browser.get(self.live_server_url + '/login/')
+        self.browser.find_element_by_id('logo')
